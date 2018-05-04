b11ead0baf4f768586e1b6477dc29c9c1a8565ca
ryoyaa <ryoya76lima@gmail.com>
2018-05-04 14:17:03 +0900

    Add validation

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,2 +1,7 @@
 class Topic < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :topics
+
+  validates :name, presence: true
+
+  enum role: {
+   normal: 0,
+   admin: 1
+  }
+
 end
