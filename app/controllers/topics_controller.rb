184fe46630b3d9645cbbcc2dce8c98c589a67037
ryoyaa <ryoya76lima@gmail.com>
2018-05-04 15:51:12 +0900

    投稿機能作成

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,14 +1,24 @@
 class TopicsController < ApplicationController
+  before_action :set_topic, only: [ :show, :edit, :update, :destroy]
+
   def index
+    @topics = Topic.all
   end
 
   def show
   end
 
   def new
+    @topic = Topic.new
   end
 
   def create
+    @topic = Topic.new(topic_params)
+    if @topic.save
+      redirect_to @topic
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +28,15 @@ class TopicsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:title, :content)
+  end
+
+  def set_topic
+    @topic = Topic.find(params[:id])
   end
 end
